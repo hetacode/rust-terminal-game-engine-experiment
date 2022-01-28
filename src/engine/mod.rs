c4eb3132ff2a32752e005e19f1ad68a442a60566
Seba G <sebag.git@gmail.com>
2022-01-28 23:45:00 +0100

    Read keyboard events in separate thread

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,53 +1,50 @@
-use termios::*;
-use std::io;
 use std::io::*;
+use std::{io, thread};
+use termios::*;
 
 pub struct Engine {}
 
 impl Engine {
     pub fn new() -> Self {
-        return Engine{};
+        return Engine {};
     }
 
     pub fn init(&self) {
-    let stdin = 0; 
-    let termios = Termios::from_fd(stdin).unwrap();
-    let mut new_termios = termios.clone();
-    new_termios.c_lflag &= !(ICANON | ECHO); // no echo and canonical mode
-    tcsetattr(stdin, TCSANOW, &mut new_termios).unwrap();
-    
-    let stdout = io::stdout();
-    let mut reader = io::stdin();
-    let mut buffer = [0; 1]; 
+        let stdin = 0;
+        let termios = Termios::from_fd(stdin).unwrap();
+        let mut new_termios = termios.clone();
+        new_termios.c_lflag &= !(ICANON | ECHO); // no echo and canonical mode
+        tcsetattr(stdin, TCSANOW, &mut new_termios).unwrap();
 
-    let (mut x, mut y) = (10, 10);
 
-    print!("\u{001B}[?25l");
-    while true {
-        print!("\u{001B}[1;1H\u{001B}[2J");
-        print!("\u{001B}[{};{}H", x, y);
-        print!("*");
-    }
+        let (mut x, mut y) = (10, 10);
 
-    print!("\u{001B}[?25h");
+        print!("\u{001B}[?25l");
+        loop {
+            print!("\u{001B}[1;1H\u{001B}[2J");
+            print!("\u{001B}[{};{}H", x, y);
+            print!("*");
+        }
+
+        print!("\u{001B}[?25h");
         tcsetattr(stdin, TCSANOW, &termios).unwrap(); // reset the stdin to
     }
-    }
+}
 
-    pub fn keyboard_events(key_callback: fn(u8)) {
-   //ctrlc::set_handler(|| {
-   //    print!("\u{001B}[?25h");
-   //    exit(0);
-   //})
-   // .unwrap();
+pub fn keyboard_events(key_callback: fn(u8)) {
+    //ctrlc::set_handler(|| {
+    //    print!("\u{001B}[?25h");
+    //    exit(0);
+    //})
+    // .unwrap();
     let stdout = io::stdout();
     let mut reader = io::stdin();
     let mut buffer = [0; 1]; // read exactly one byte
-    loop {
+    thread::spawn(move || loop {
         stdout.lock().flush().unwrap();
         reader.read_exact(&mut buffer).unwrap();
-        if (!buffer.is_empty()) {
+        if !buffer.is_empty() {
             key_callback(buffer[0])
         }
-    }
+    });
 }
