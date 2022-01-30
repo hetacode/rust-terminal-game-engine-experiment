e4dc9800d05e2160a3a86bb1e56e8900d1f1fe86
Seba G <sebag.git@gmail.com>
2022-01-30 12:51:08 +0100

    Engine draw and keyboard functions

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,50 +1,52 @@
 use std::io::*;
 use std::{io, thread};
 use termios::*;
-
-pub struct Engine {}
+pub struct Engine {
+    draw_callback: Option<fn() -> ()>,
+}
 
 impl Engine {
     pub fn new() -> Self {
-        return Engine {};
+        return Engine {
+            draw_callback: None,
+        };
     }
 
-    pub fn init(&self) {
+    pub fn init(&mut self) {
         let stdin = 0;
         let termios = Termios::from_fd(stdin).unwrap();
         let mut new_termios = termios.clone();
         new_termios.c_lflag &= !(ICANON | ECHO); // no echo and canonical mode
         tcsetattr(stdin, TCSANOW, &mut new_termios).unwrap();
 
-
-        let (mut x, mut y) = (10, 10);
-
         print!("\u{001B}[?25l");
         loop {
-            print!("\u{001B}[1;1H\u{001B}[2J");
-            print!("\u{001B}[{};{}H", x, y);
-            print!("*");
+            self.draw_callback.unwrap()();
+            //thread::sleep(std::time::Duration::from_millis(16));
         }
-
         print!("\u{001B}[?25h");
         tcsetattr(stdin, TCSANOW, &termios).unwrap(); // reset the stdin to
     }
-}
 
-pub fn keyboard_events(key_callback: fn(u8)) {
-    //ctrlc::set_handler(|| {
-    //    print!("\u{001B}[?25h");
-    //    exit(0);
-    //})
-    // .unwrap();
-    let stdout = io::stdout();
-    let mut reader = io::stdin();
-    let mut buffer = [0; 1]; // read exactly one byte
-    thread::spawn(move || loop {
-        stdout.lock().flush().unwrap();
-        reader.read_exact(&mut buffer).unwrap();
-        if !buffer.is_empty() {
-            key_callback(buffer[0])
-        }
-    });
+    pub fn draw(&mut self, draw_callback: fn() -> ()) {
+        self.draw_callback = Some(draw_callback);
+    }
+
+    pub fn keyboard_events(key_callback: fn(u8)) {
+        //ctrlc::set_handler(|| {
+        //    print!("\u{001B}[?25h");
+        //    exit(0);
+        //})
+        // .unwrap();
+        let stdout = io::stdout();
+        let mut reader = io::stdin();
+        let mut buffer = [0; 1]; // read exactly one byte
+        thread::spawn(move || loop {
+            stdout.lock().flush().unwrap();
+            reader.read_exact(&mut buffer).unwrap();
+            if !buffer.is_empty() {
+                key_callback(buffer[0])
+            }
+        });
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod engine;
 fn main() {
-    let e = engine::Engine::new();
+    let mut e = engine::Engine::new();
+    e.draw(|| {
+        let (mut x, mut y) = (10, 10);
+        print!("\u{001B}[1;1H\u{001B}[2J");
+        print!("\u{001B}[{};{}H", x, y);
+        print!("*");
+    });
     e.init(); 
 }
