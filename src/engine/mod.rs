ba5415c0fa9cec4620395192c8e5bcb19d89f29e
Seba G <sebag.git@gmail.com>
2022-01-30 21:51:28 +0100

    Move point by keyboard - in two threads

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,8 +1,9 @@
 use std::io::*;
 use std::{io, thread};
 use termios::*;
+
 pub struct Engine {
-    draw_callback: Option<fn() -> ()>,
+    draw_callback: Option<Box<dyn Fn()>>,
 }
 
 impl Engine {
@@ -21,18 +22,18 @@ impl Engine {
 
         print!("\u{001B}[?25l");
         loop {
-            self.draw_callback.unwrap()();
-            //thread::sleep(std::time::Duration::from_millis(16));
+            self.draw_callback.as_ref().unwrap()();
+            thread::sleep(std::time::Duration::from_millis(16));
         }
         print!("\u{001B}[?25h");
         tcsetattr(stdin, TCSANOW, &termios).unwrap(); // reset the stdin to
     }
 
-    pub fn draw(&mut self, draw_callback: fn() -> ()) {
-        self.draw_callback = Some(draw_callback);
+    pub fn draw(&mut self, draw_callback: impl Fn() -> () + 'static) {
+        self.draw_callback = Some(Box::new(draw_callback));
     }
 
-    pub fn keyboard_events(key_callback: fn(u8)) {
+    pub fn keyboard_events(&mut self, mut key_callback: impl FnMut(u8) + Send + 'static) {
         //ctrlc::set_handler(|| {
         //    print!("\u{001B}[?25h");
         //    exit(0);
@@ -45,7 +46,7 @@ impl Engine {
             stdout.lock().flush().unwrap();
             reader.read_exact(&mut buffer).unwrap();
             if !buffer.is_empty() {
-                key_callback(buffer[0])
+                 key_callback(buffer[0]);
             }
         });
     }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
 mod engine;
+
+struct Game {
+    pub x: i32,
+    pub y: i32,
+}
+
 fn main() {
     let mut e = engine::Engine::new();
-    e.draw(|| {
-        let (mut x, mut y) = (10, 10);
+    let data = Arc::new(Mutex::new(Game { x: 10, y: 10 }));
+
+    let d = data.clone();
+    e.draw(move || {
+        let d = d.lock().unwrap();
         print!("\u{001B}[1;1H\u{001B}[2J");
-        print!("\u{001B}[{};{}H", x, y);
+        print!("\u{001B}[{};{}H", d.y, d.x);
         print!("*");
     });
-    e.init(); 
+
+    let d = data.clone();
+    e.keyboard_events(move |key_code| {
+        let mut d = d.lock().unwrap();
+        match key_code {
+            100 => d.x = d.x + 1,
+            115 => d.y = d.y + 1,
+            97 => d.x = d.x - 1,
+            119 => d.y = d.y - 1,
+            _ => {}
+        }
+    });
+
+    e.init();
 }
